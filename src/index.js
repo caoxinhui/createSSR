ab8e6f6c8116ec49e93c6f5277f2367bf90cc7f1
cxh曹昕卉 <caoxh@ctrip.com>
2019-04-18 16:28:16 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import createApp from "./server";
+
+import ReactDOM from "react-dom";
+import routes from "./routes";
+
+const webpackLoader = async loadModule => {
+  let module = await new Promise(loadModule);
+  return module.default || module;
+};
+
+const viewEngine = {
+  render: (component, container) => {
+    return ReactDOM.render(component, container);
+  }
+};
+
+const app = createApp({
+  type: "createHistory",
+  basename: "/examples/simple-spa",
+  container: "#container",
+  routes: routes,
+  loader: webpackLoader,
+  viewEngine: viewEngine
+});
+app.start();
--- src/util.js
+++ src/util.js
@@ -1,7 +1,50 @@
+import { type } from "os";
+
 export function extend(to, from) {
-    if(!from){
-        return to
+  if (!from) {
+    return to;
+  }
+  var keys = Object.keys(from);
+  var i = keys.length;
+  while (i--) {
+    to[keys[i]] = from[keys[i]];
+  }
+  return to;
+}
+export function createMap() {
+  let list = [];
+  function find(key) {
+    return list.filter(item => item.key === key);
+  }
+  function has(key) {
+    let result = find(key);
+    return result.length > 0;
+  }
+  function get(key) {
+    let result = find(key);
+    result.length ? result[0].value : undefined;
+  }
+  function set(key, value) {
+    let result = find(key);
+    if (result.length === 0) {
+      result = { key, value };
+      list.push(result);
+    } else {
+      result.value = value;
     }
+  }
+  function remove(key) {
+    list = list.filter(item => item.key !== key);
+  }
+  return { get, set, has, remove };
+}
+
+export function isThenable(obj) {
+  return obj != null && typeof obj.then === "function";
+}
+
+export function extend(to,from){
+    if(!from) return to
     var keys = Object.keys(from)
     var i = keys.length
     while(i--){
