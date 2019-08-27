ef64bdc6f959edefa3fbc3b2a8da47a64708d5bf
caoxinhui <43514495+caoxinhui@users.noreply.github.com>
2019-08-27 09:32:11 +0800

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Home from '../containers/Home'
+const app = express()
+const content = renderToString(<Home />)
+app.get('/', function (req, res) {
+  res.send(
+    `<html>
+    <head>
+      <title>ssr</title>
+    </head>
+    <body>
+      <div id="root">${content}</div>
+    </body>
+  </html>`
+  )
+})
+app.listen(3001, () => {
+  console.log('listen on 3001')
+})
